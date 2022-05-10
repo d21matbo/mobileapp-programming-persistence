47498d525b1f2aae8f11e1ed5a8bef298471577f
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-10 16:09:40 +0200

    Update variable names and add sql insert method

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,9 +13,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView displayView;
     private Button buttonRead;
     private Button buttonWrite;
-    private EditText input1;
-    private EditText input2;
-    private EditText input3;
+    private EditText inputLicNo;
+    private EditText inputBrand;
+    private EditText inputModel;
+
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +25,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViews();
+        databaseHelper = new DatabaseHelper(this);
     }
 
     private void findViews() {
         displayView = findViewById(R.id.display_sqldata);
         buttonRead = findViewById(R.id.button_read);
         buttonWrite = findViewById(R.id.button_write);
-        input1 = findViewById(R.id.input_1);
-        input2 = findViewById(R.id.input_2);
-        input3 = findViewById(R.id.input_3);
+        inputLicNo = findViewById(R.id.input_licno);
+        inputBrand = findViewById(R.id.input_brand);
+        inputModel = findViewById(R.id.input_model);
+    }
+
+    private void insertSQLData() {
+        ContentValues values = new ContentValues();
+        values.put(DatabaseTables.Car.COLUMN_LICNO, inputLicNo.getText().toString());
+        values.put(DatabaseTables.Car.COLUMN_BRAND, inputBrand.getText().toString());
+        values.put(DatabaseTables.Car.COLUMN_MODEL, inputModel.getText().toString());
+        databaseHelper.getWritableDatabase().insert(DatabaseTables.Car.TABLE_NAME, null, values);
     }
 }
